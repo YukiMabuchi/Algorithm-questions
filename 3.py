8b51234db9cf9a33598ebdfdf66a1ff0f00f209f
Yuki Mabuchi <67894806+YukiMabuchi@users.noreply.github.com>
2022-05-03 12:31:54 +0900

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,14 +1,3 @@
-#https://gist.github.com/ilivessevili/8338697d75a26240b20e
-#下のほうがいい
-#https://www.bswen.com/2018/04/python-How-to-generate-random-large-file-using-python.html
-
-# import os
-
-# #３回かけるとMBのサイズになる
-# MB1 = 1024*1024*1024
-# with open('large_file', 'wb') as fout:
-#     fout.write(os.urandom(MB1)) 
-
 file_size_MB = int(input('Plese enter the size of the file(MB) : '))
  
 def generate_file(filename, size_MB):
@@ -25,13 +14,3 @@ def generate_file(filename, size_MB):
 
 if __name__ == '__main__':
     generate_file("test_file", file_size_MB)
-
-
-#   import os 
-#     with open('%s'%filename, 'wb') as fout:
-#         fout.write(os.urandom(size)) #1
-#     print ('big random binary file with size %f generated ok'%size)
-#     pass
-
-    # print ('big random binary file with size %f generated ok'%int(file_size_hex, 16))
-
